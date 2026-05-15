ca6ffe55f1cf24cbd07bd15d264671b928b241e3
ratnathuon <ratnathuon@gmail.com>
2026-05-15 17:19:57 +0700

    About u page and fix destinations cards

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,13 +5,15 @@ import Footer from '../components/Footer'
 export default function About() {
   return (
     <div>
-      <h1 className='mt-10 text-center from-neutral-950 text-3xl '>Team Member</h1>
-        <div className=' flex gap-x-3'>
+      <h1 className='font-poppins font-bold mt-10 text-center text-green-900 text-5xl '>Team Member</h1>
+      <p className=' text-center mt-10 italic text-2xl'>We’re Student of RUPP</p>
+        <div className='pl-44 pr-44 justify-between flex mt-0'>
           <AboutUsCard/>
           <AboutUsCard/>
           <AboutUsCard/>
           <AboutUsCard/>
         </div>
+      <p className=' mb-10 font-poppins text-white p-80 pt-16 pb-16 text-center text-lg bg-gradient-to-r from-[#0F2027] via-[#28623a] to-[#28623a]  '>Powered by a team of IT Engineering students at RUPP, Travel Cambodia is a digital initiative dedicated to showcasing the beauty of our home. We combine engineering precision with local insight to help you navigate the Kingdom of Wonder effortlessly.</p>
       <Footer/>
     </div>
     
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,13 +5,15 @@ import Destinations from '../components/Destinations'
 import SliderHeader from '../components/SlideHeader'
 import Category from '../components/Category'
 import HeroSection from '../components/HeroSection'
+import TopPlace from '../components/TopPlace'
 export default function Home() {
   return (
     <div>
       <Search />
       <SliderHeader/>
       <h1 className='text-center font-bold text-5xl m-8 text-green-900 font-poppins italic'>Top Destinations</h1>
-      <Destinations />
+      {/* <Destinations /> */}
+      <TopPlace/>
       <HeroSection/>
       <h1 className='text-center font-bold text-5xl m-8 text-green-900 font-poppins italic'>Top Explore in Cambodia</h1>
       <Category/>
